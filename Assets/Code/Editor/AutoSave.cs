ab179f21de6b6bcfe0ae46ce98d459b14330d035
hardlydifficultllc <hardlydifficultllc@gmail.com>
2017-07-28 17:36:45 -0700

    Base project.

---
+++ Assets/Code/Editor/AutoSave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutoSave
+{
+  static AutoSave()
+  {
+    EditorApplication.playmodeStateChanged
+      += OnPlaymodeStateChanged;
+  }
+
+  static void OnPlaymodeStateChanged()
+  {
+    if(EditorApplication.isPlaying)
+    {
+      return;
+    }
+
+    EditorSceneManager.SaveOpenScenes();
+  }
+}
